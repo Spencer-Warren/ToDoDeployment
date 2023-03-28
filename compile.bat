6d3e228e66861c03734e8131aafb13add362f85e
Spencer A Warren <75763632+Spencer-Warren@users.noreply.github.com>
2023-03-28 14:56:29 -0400

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-cd FrontEnd && ng build --delete-output-path --output-path ../BackEnd/src/main/resources/public && cd ../BackEnd && mvn clean install
+cd FrontEnd && npm install && ng build --delete-output-path --output-path ../BackEnd/src/main/resources/public && cd ../BackEnd && mvn clean install
