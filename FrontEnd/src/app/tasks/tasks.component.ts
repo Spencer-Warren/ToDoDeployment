0c2e8ad709ab5f4076333f80fb803a9e7266a404
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-03-31 15:39:47 -0400

    Tasks sort

--- FrontEnd/src/app/tasks/tasks.component.ts
+++ FrontEnd/src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Task } from '../classes/task';
 import { Router } from '@angular/router';
 import { RESTAPIService } from '../services/restapiservice.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
@@ -10,13 +11,22 @@ import { RESTAPIService } from '../services/restapiservice.service';
 })
 export class TasksComponent {
   tasks: Task[] = [];
+  observableTasks: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>([]);
 
   constructor(private router: Router, private service: RESTAPIService) {
     this.getTasks();
   }
 
+  get tasksList(): Observable<Task[]> {
+    return this.observableTasks.asObservable();
+  }
+
   getTasks() {
-    this.service.getTasks().subscribe((data: any) => { this.tasks = data });
+    this.service.getTasks().subscribe((data: any) => {
+      this.tasks = data
+      // put the tasks in the observable
+      this.observableTasks.next(this.tasks);
+    });
   }
 
   createTask() {
@@ -29,5 +39,22 @@ export class TasksComponent {
 
   deleteTask(taskId: number) {
     this.service.deleteTask(taskId).subscribe((data: any) => { this.getTasks() });
+  }
+
+  sort(col : string) {
+    switch (col) {
+      case 'date':
+        this.tasks.sort((a, b) => { return a.dueDate.localeCompare(b.dueDate)});
+        break;
+      case 'title':
+        this.tasks.sort((a, b) => { return a.title.localeCompare(b.title)});
+        break;
+      case 'status':
+        this.tasks.sort((a, b) => { return a.status.localeCompare(b.status)});
+        break;
+      case 'description':
+        this.tasks.sort((a, b) => { return a.description.localeCompare(b.description)});
+        break;
+    }
   }
 }
